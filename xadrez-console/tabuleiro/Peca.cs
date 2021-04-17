c8d6d5a2f9211bcd55c1453fb10cbdb8a83ad35c
Edison Junior <mage1232010@gmail.com>
2021-04-17 12:50:56 -0300

    Testes iniciais com movimentos possiveis das peças

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿namespace tabuleiro
 {
-    internal class Peca
+    internal abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -20,5 +20,7 @@
         {
             qtdeMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
